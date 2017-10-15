44fb80b6fbf5f2286ce8931705448bc8a1f20f75
yixiao1 <32467324+yixiao1@users.noreply.github.com>
2017-10-15 19:40:10 +0200

    Add files via upload

---
+++ week2/Task3/readme.m
@@ -0,0 +1,16 @@
+%{
+Juan Felipe Montesinos
+Yi Xiao
+Ferran Carrasquer Mas
+Master in Computer Vision
+Computer Vision Center, Barcelona
+---------------------------
+Project M1/Block2
+---------------------------
+
+To see the results in task3, you can display a mask_image, and then call 
+a function named MorphologicalTransform.m to make the morphological 
+transform. finally you can compare the before mask_image and the after one
+ 
+
+%}
